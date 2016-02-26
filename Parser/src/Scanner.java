6da0d9042f774f72e6d8e053c6b852c208afc7a1
amirkhan29 <amirkhan@gatech.edu>
2016-02-26 05:47:16 -0500

    Added files via upload
    Contains the scanner Implementation
    Need to correct the implementation for reading floating point value

---
+++ Parser/src/Scanner.java
@@ -0,0 +1,125 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		// String fileName = args[0];
+		String fileName = "factorial.tgr";
+		String fullFileText = "";
+        String line = null;
+
+	    try {
+            FileReader fileReader = new FileReader(fileName);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            while((line = bufferedReader.readLine()) != null) {
+                fullFileText += line + "\n";
+            }   
+            bufferedReader.close();         
+        } catch(FileNotFoundException ex) {
+        	System.out.println("Unable to open file '" + fileName + "'");                
+        } catch(IOException ex) {
+            System.out.println("Error reading file '" + fileName + "'");                  
+            // ex.printStackTrace();
+        }
+		   String wordlist[] = {"array", "begin","break", "do", "else", "end", "enddo", "endif","float",
+				   "for", "func", "if", "in", "int", "let", "of", "return","then", "to", "type", "var",
+				   "while",",", ":", ";","(", ")","[" ,"]" ,"{" ,"}" ,"." ,"+" ,"-", "*", "/", "=","<>",
+				   		"<",">","<=",">=","&","|",":="};    
+	  
+
+	    //String fullFileText1[] = (fullFileText.split("((?<=,)|(?=,)|(?<=;)|(?=;)|(?<=:)|(?=:))"));
+	    String fullFileText_token[] = (fullFileText.split("(\\s+)|((?=,)|(?=;)|(?=:)|(?<=\\()|(?=\\()|(?=\\))|(?<=\\)))"));
+	    
+	   //  String fullFileText2 = Arrays.toString(fullFileText1);
+	    	//	System.out.println(Arrays.toString(fullFileText1));
+	   for (int i=0; i<200; i++)
+	   {
+    		//System.out.println(fullFileText_token[i]);
+	    	          
+	   String token = fullFileText_token[i];
+  		//System.out.println(token);
+// Removing remaining Whitespaces	   
+	   Boolean whitespace_match = false;
+   if(token.trim().length() <= 0)
+		   {
+		   	//	System.out.println("Whitespace:" +token); 
+		   		whitespace_match = true; 
+		   }		   			   
+	   
+// Comparing the token with keywords
+		Boolean keyword_match = false;   
+		   if(whitespace_match == false)
+		   {
+		for (String word:wordlist)
+	   {
+		   if(word.equals(token)== true)
+	     {
+			   System.out.println("Keyword: "+word);
+			   keyword_match = true;
+			     break;
+	     }		   
+	   }
+		   }
+	   
+
+		   
+// Comparing token for identifier
+	   		//System.out.println("Keyword match: " +keyword_match); 
+	   	//	System.out.println("Whitespace Match: "+whitespace_match);   
+		   
+			Boolean identifier_match = false; 
+		   if((keyword_match == false) & (whitespace_match == false))
+		   {
+			   if(Character.toString(token.charAt(0)).matches("[A-Z?]|[a-z?]"))
+			   {
+				   if(token.matches("[_]+|[A-Z?]+|[a-z?]+|[0-9?]+"))
+				   {
+				   		System.out.println("Id: "+token); 
+						 identifier_match = true;   
+				   }
+
+			   }
+			   else if(Character.toString(token.charAt(0)).matches("_"))
+			   {
+				   if(token.matches("[_]&([A-Z?]|[a-z?]|[0-9?])"))
+				   {
+				   		System.out.println("Id: "+token);  
+						 identifier_match = true;
+				   }						   		
+			   }
+			   else
+			   {
+				   // Error
+			   }
+		   }
+		   
+	// Comparing token for integer literals	
+		   Boolean intliteral_match = false;
+		   if((keyword_match == false) & (whitespace_match == false) & (identifier_match== false))
+		   {
+			   if(token.matches("[0-9?]+"))
+			   {
+			   		System.out.println("Intlit: "+token); 
+			   		intliteral_match = true; 
+			   }
+		   }
+
+// Comparing token to float Literals
+		   Boolean floatliteral_match = false;
+		   if((keyword_match == false) & (whitespace_match == false) & (identifier_match== false)
+				   & (intliteral_match== false))
+		   { System.out.println("floatlit: Entered ");
+			   if(token.matches("[0-9?]+ | ."))
+			   {
+					System.out.println("floatlit: "+token);
+			   }
+		   }
+	   
+	   }	      
+	}
+
+}
