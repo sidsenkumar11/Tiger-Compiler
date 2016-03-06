800081cd4ae42430bcce3ad5796919a3871fbfa5
amirkhan29 <amirkhan@gatech.edu>
2016-03-06 05:15:37 -0500

    Update Scanner.java
    Comparing one character at a time

--- Compiler/src/Scanner/Scanner.java
+++ Compiler/src/Scanner/Scanner.java
@@ -2,107 +2,149 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 
-public class Scanner {
+public class Runner {
 
-    public static void main(String[] args) {
-
-        /* Read in file into string of text */
-        // String fileName = args[0];
-        String fileName = "tests/factorial.tgr";
-        String fullFileText = "";
+	public static void main(String[] args) {
+		// String fileName = args[0];
+		String fileName = "factorial.tgr";
+		String fullFileText = "";
         String line = null;
 
-        try {
+	    try {
             FileReader fileReader = new FileReader(fileName);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
-            while ((line = bufferedReader.readLine()) != null) {
+            while((line = bufferedReader.readLine()) != null) {
                 fullFileText += line + "\n";
-            }
-            bufferedReader.close();
-        } catch (FileNotFoundException ex) {
-            System.out.println("Unable to open file '" + fileName + "'");
-        } catch (IOException ex) {
-            System.out.println("Error reading file '" + fileName + "'");
+            }   
+            bufferedReader.close();         
+        } catch(FileNotFoundException ex) {
+        	System.out.println("Unable to open file '" + fileName + "'");                
+        } catch(IOException ex) {
+            System.out.println("Error reading file '" + fileName + "'");                  
             // ex.printStackTrace();
         }
+		   String wordlist[] = {"array", "begin","break", "do", "else", "end", "enddo", "endif","float",
+				   "for", "func", "if", "in", "int", "let", "of", "return","then", "to", "type", "var",
+				   "while",",", ":", ";","(", ")","[" ,"]" ,"{" ,"}" ,"." ,"+" ,"-", "*", "/", "=","<>",
+				   		"<",">","<=",">=","&","|",":="};    
+	  
 
-        /* Begin scanner code */
-        String wordlist[] = {"array", "begin", "break", "do", "else", "end", "enddo", "endif", "float",
-                "for", "func", "if", "in", "int", "let", "of", "return", "then", "to", "type", "var",
-                "while", ",", ":", ";", "(", ")", "[", "]", "{", "}", ".", "+", "-", "*", "/", "=", "<>",
-                "<", ">", "<=", ">=", "&", "|", ":="};
+	    //String fullFileText1[] = (fullFileText.split("((?<=,)|(?=,)|(?<=;)|(?=;)|(?<=:)|(?=:))"));
+	    String fullFileText_token[] = (fullFileText.split("(\\s+)|((?=,)|(?=;)|(?=:)|(?<=\\()|(?=\\()|(?=\\))|(?<=\\)))"));
+	    
+	    	//	System.out.println(fullFileText_token.length);
+	   //for (int i=0; i< fullFileText_token.length; i++)
+	    for (int i=0; i<fullFileText_token.length; i++)  
+	   {
+    		//System.out.println(fullFileText_token[i]);
+	    	          
+	   String token = fullFileText_token[i];
+// Removing remaining Whitespaces	   
+	   Boolean whitespace_match = false;
+   if(token.trim().length() <= 0)
+		   {
+		   	//	System.out.println("Whitespace:" +token); 
+		   		whitespace_match = true; 
+		   }		   			   
+	   
+// Comparing the token with keywords
+		Boolean keyword_match = false;   
+	if(whitespace_match == false)
+	{		
+	for (String word:wordlist)
+	   {
+		if((token.length() == word.length()) & keyword_match == false)
+			{
+			for (int j = 0; j < word.length() ; j++)			   				   
+				{
+				// System.out.println(token.charAt(j));
+				if((word.charAt(j))==(token.charAt(j)))
+				   	{ 			  
+					// 	System.out.println("char: "+word.charAt(j));
+					if(j ==  word.length()-1)
+					   {
+					   	keyword_match = true;
+					   //	System.out.println(keyword_match);
+					   	System.out.println("keyword: "+token);	
+					   }
+				   	}	
+				   else
+				   {
+					 	 break;
+				   }
+				}
+			}
+		}
+	   
+		// Comparing token for identifier  		   
+			Boolean identifier_match = false; 
+		   if((keyword_match == false) & (whitespace_match == false))
+		   {
+			   if(Character.toString(token.charAt(0)).matches("[A-Z?]|[a-z?]"))
+			   {
+ 
+				   for (int j = 0; j < token.length(); j++)
+				   {
+				   if(Character.toString(token.charAt(j)).matches("[_]+|[A-Z?]+|[a-z?]+|[0-9?]+"))
+				   {						  
+				   		if(j ==  token.length()-1)
+						   {
+				   		System.out.println("Id: "+token);
+						 identifier_match = true;  
+						   }
+				   }						   
+				   }
 
-
-        //String fullFileText1[] = (fullFileText.split("((?<=,)|(?=,)|(?<=;)|(?=;)|(?<=:)|(?=:))"));
-        String fullFileText_token[] = (fullFileText.split("(\\s+)|((?=,)|(?=;)|(?=:)|(?<=\\()|(?=\\()|(?=\\))|(?<=\\)))"));
-
-        //  String fullFileText2 = Arrays.toString(fullFileText1);
-        //	System.out.println(Arrays.toString(fullFileText1));
-        for (int i = 0; i < fullFileText_token.length; i++) {
-            //System.out.println(fullFileText_token[i]);
-            String token = fullFileText_token[i];
-            //System.out.println(token);
-            // Removing remaining Whitespaces
-            boolean whitespace_match = false;
-            if (token.trim().length() <= 0) {
-                //	System.out.println("Whitespace:" +token);
-                whitespace_match = true;
-            }
-
-            // Comparing the token with keywords
-            boolean keyword_match = false;
-            if (whitespace_match == false) {
-                for (String word : wordlist) {
-                    if (word.equals(token) == true) {
-                        System.out.println("Keyword " + word);
-                        keyword_match = true;
-                        break;
-                    }
-                }
-            }
-
-
-            // Comparing token for identifier
-            //System.out.println("Keyword match: " +keyword_match);
-            //	System.out.println("Whitespace Match: "+whitespace_match);
-
-            boolean identifier_match = false;
-            if ((keyword_match == false) & (whitespace_match == false)) {
-                if (Character.toString(token.charAt(0)).matches("[A-Z?]|[a-z?]")) {
-                    if (token.matches("[_]+|[A-Z?]+|[a-z?]+|[0-9?]+")) {
-                        System.out.println("Id " + token);
-                        identifier_match = true;
-                    }
-
-                } else if (Character.toString(token.charAt(0)).matches("_")) {
-                    if (token.matches("[_]&([A-Z?]|[a-z?]|[0-9?])")) {
-                        System.out.println("Id " + token);
-                        identifier_match = true;
-                    }
-                } else {
-                    // Error
-                }
-            }
-
-            // Comparing token for integer literals
-            boolean intliteral_match = false;
-            if ((keyword_match == false) & (whitespace_match == false) & (identifier_match == false)) {
-                if (token.matches("[0-9?]+")) {
-                    System.out.println("Intlit " + token);
-                    intliteral_match = true;
-                }
-            }
-
-            // Comparing token to float Literals
-            boolean floatliteral_match = false;
-            if ((keyword_match == false) & (whitespace_match == false) & (identifier_match == false)
-                    & (intliteral_match == false)) {
-                System.out.println("floatlit? Entered ");
-                if (token.matches("[0-9?]+ | .")) {
-                    System.out.println("floatlit " + token);
-                }
-            }
-        }
-    }
+			   }
+			   else if(Character.toString(token.charAt(0)).matches("_"))
+			   {
+				   for (int j = 1; j < token.length(); j++)
+				   {
+				   if(Character.toString(token.charAt(j)).matches("([A-Z?]|[a-z?]|[0-9?])+"))
+				   {
+				   		if(j ==  token.length()-1)
+						   {
+				   		System.out.println("Id: "+token);  
+						 identifier_match = true;
+						   }
+				   }
+				   }
+			   }
+			   else
+			   {
+				   // Error
+			   }
+		   }
+		   		   
+		 
+			// Comparing token for integer literals	
+		   Boolean intliteral_match = false;
+		   if((keyword_match == false) & (whitespace_match == false) & (identifier_match== false))
+		   {
+			   if(token.matches("[0-9?]+"))
+			   {
+			   		System.out.println("Intlit: "+token); 
+			   		intliteral_match = true; 
+			   }
+		   }
+		   
+		// Comparing token to float Literals
+		   Boolean floatliteral_match = false;
+		   if((keyword_match == false) & (whitespace_match == false) & (identifier_match== false)
+				   & (intliteral_match== false))
+		   { 
+			   if(token.matches("([0-9]*)\\.[0]"))
+			   {
+					System.out.println("floatlit: "+token);
+			   }
+		   }		   
+		   
+		   
+		   }
+	   }
+	}
 }
+	   
+
