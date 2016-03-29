f071ae5b0267d61876fc5f0976b555ee65554144
Nick Mathews <nick@codemettle.com>
2016-03-29 18:40:17 -0400

    print tokens when requested

--- Compiler/src/antlr/tiger.java
+++ Compiler/src/antlr/tiger.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class tiger {
@@ -19,19 +21,20 @@ public class tiger {
     }
 
     private static String helpMessage =
-        "Tiger Phase I Reference Implementation for CS 4240\n" +
-        "Flags:\n" +
-        "    -h, --help    :    shows help message\n" +
-        "    --ast         :    outputs syntax tree to stdout\n" +
-        "    --tokens      :    prints token stream (unsupported by reference\n" +
-        "                       implementation, but you need to support it)\n" +
-        "    -g, --gui     :    displays graphical image of tree";
+            "Tiger Phase I Reference Implementation for CS 4240\n" +
+                    "Flags:\n" +
+                    "    -h, --help    :    shows help message\n" +
+                    "    --ast         :    outputs syntax tree to stdout\n" +
+                    "    --tokens      :    prints token stream (unsupported by reference\n" +
+                    "                       implementation, but you need to support it)\n" +
+                    "    -g, --gui     :    displays graphical image of tree";
 
 
 
     public static void main(String[] args){
 
         String sourceCode = null;
+        String sourceFile = null;
         boolean helpFlag = false;
         boolean tokensFlag = false;
         boolean astFlag = false;
@@ -39,6 +42,7 @@ public class tiger {
 
         for (String arg : args){
             if (arg.matches("(.*).tgr")){
+                sourceFile = arg;
                 try {
                     sourceCode = readFile(arg);
                 }
@@ -72,17 +76,29 @@ public class tiger {
 
         TigerLexer lexer = new TigerLexer(new ANTLRInputStream(sourceCode));
         CommonTokenStream tokens = new CommonTokenStream(lexer);
+
         TigerParser parser = new TigerParser(tokens);
         TigerParser.ProgramContext tree = parser.program();
 
         ParseTreeWalker walker = new ParseTreeWalker();
         TigerProgramListener listener = new TigerProgramListener();
         walker.walk(listener, tree);
-        
+
         TypeChecker.typeCheck(tree.toStringTree(parser).toLowerCase());
 
         if (tokensFlag){
-            System.out.println("--tokens unsupported by reference implementation. You still have to support it.");
+
+            DFAScanner scanner = new DFAScanner();
+            List<Token> tok = scanner.scan(sourceFile);
+            for(Token t : tok) {
+                if(t.getType().equalsIgnoreCase("intlit") || t.getType().equalsIgnoreCase("floatlit")) {
+                    System.out.print(t.getContent() + ":" + t.getType().toLowerCase());
+                } else {
+                    System.out.print(t + " ");
+                }
+            }
+            System.out.println();
+//            System.out.println("--tokens unsupported by reference implementation. You still have to support it.");
         }
         if (astFlag){
             System.out.println(tree.toStringTree(parser).toLowerCase());
