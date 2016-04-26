029f74337809d902d65d60d2693de0d84f12d77a
Siddarth Senthilkumar <ssenthilkumar3@gatech.edu>
2016-04-26 15:11:35 -0400

    Evaluate Array Expressions

--- Compiler/src/IntermediateCode/EvaluateExpression.java
+++ Compiler/src/IntermediateCode/EvaluateExpression.java
@@ -5,12 +5,14 @@ public class EvaluateExpression {
     public static ArrayList<String> evaluateExpression(String expression, int origReg)  {
 
         int reg = origReg;
+        ArrayList<String> instructions = new ArrayList<>();
 
         // Get the expression from the ast
         String[] tokens = expression.split(" ");
+        System.out.println(java.util.Arrays.toString(tokens));
         String trueExpression = "";
         int i = 0;
-        while (!tokens[i].contains(";")) {
+        while (i < tokens.length && !tokens[i].contains(";")) {
             if (tokens[i].contains("factor")) {
                 // Next token is either "const" or is variable name
                 if (tokens[i+1].contains("const")) {
@@ -22,6 +24,37 @@ public class EvaluateExpression {
                     trueExpression += theNum + " ";
                 } else {
                     // Variable name
+                    String theVariableName = "";
+                    for (int j = 0; j < tokens[i+1].length() && tokens[i+1].charAt(j) != ')'; j++) {
+                        theVariableName += tokens[i+1].charAt(j);
+                    }
+
+                    // Either a pure variable or an array variable
+                    if (i + 2 < tokens.length && tokens[i+2].equals("[")) {
+                        // TODO: Replace array variable with value
+                        // It is an array variable
+                        // Get the index
+                        String indexExpression = "";
+                        int currentIndex = i+2;
+                        while (!tokens[currentIndex].contains("]")) {
+                            indexExpression += tokens[currentIndex++] + " ";
+                        }
+
+                        // Add instructions to calculate index
+//                        System.out.println("Index expression: " + indexExpression);
+                        ArrayList<String> indexCalculations = evaluateExpression(indexExpression, reg);
+                        instructions.addAll(indexCalculations);
+                        reg++; // We just used a register to hold the index
+//                        System.out.println(indexCalculations);
+
+                        // If array holds ints, put into int register, else float
+                        int regOfArrayBase = 0; // TODO: Fill with array's base register
+                        if (true) {
+                            instructions.add("loadarri r" + regOfArrayBase + "i " + "r" + (reg - 1) + "i r" + (reg++) + "i");
+                        } else {
+                            instructions.add("loadarrf r" + regOfArrayBase + "i " + "r" + (reg - 1) + "i r" + (reg++) + "f");
+                        }
+                    }
                     // TODO: Replace variable name with value
                     String num = "1";
                     trueExpression += num + " ";
@@ -38,44 +71,163 @@ public class EvaluateExpression {
         }
 
         // Evaluate multiplication and division, store results
-        ArrayList<String> instructions = new ArrayList<>();
         String[] symbols = trueExpression.split(" ");
 
         // Evaluate parenthesized expressions
         // TODO:
 
+
+        boolean notJustOne = false;
         for (i = 0; i < symbols.length; i++) {
             // Find any mults/divs
             if (symbols[i].equals("*")) {
-                try {
-                    Integer.parseInt(symbols[i - 1]);
-                    Integer.parseInt(symbols[i + 1]);
-                    instructions.add("multdubimmi r" + (reg++) + "i " + symbols[i - 1] + " " + symbols[i + 1]);
-                    symbols[i-1] = "r" + (reg - 1) + "i";
-                } catch (Exception e) {
-                    instructions.add("multdubimmf r" + (reg++) + "f " + symbols[i - 1] + " " + symbols[i + 1]);
-                    symbols[i-1] = "r" + (reg - 1) + "f";
+                notJustOne = true;
+                String termOne = symbols[prevSymbol(symbols, i)];
+                String termTwo = symbols[nextSymbol(symbols, i)];
+
+                if (termOne.contains("r") && termTwo.contains("r")) {
+                    // Both are registers
+                    if (termOne.contains("i") && termTwo.contains("i")) {
+                        // Both are int registers so int operation
+                        instructions.add("multi r" + (reg++) + "i " + termOne + " " + termTwo);
+                        symbols[nextSymbol(symbols, i)] = "r" + (reg - 1) + "i";
+                    } else {
+                        // One is a float register so result is a float reg.
+                        instructions.add("multf r" + (reg++) + "f " + termOne + " " + termTwo);
+                        symbols[nextSymbol(symbols, i)] = "r" + (reg - 1) + "f";
+                    }
+                    symbols[prevSymbol(symbols, i)] = "";
+                    symbols[i] = "";
+                } else if (termOne.contains("r")) {
+                    // One is a register
+                    if (termOne.contains("i") && (Double.parseDouble(termTwo) == (int) Double.parseDouble(termTwo))) {
+                        // Both are ints so int operation
+                        instructions.add("multimmi r" + (reg++) + "i " + termOne + " " + termTwo);
+                        symbols[nextSymbol(symbols, i)] = "r" + (reg - 1) + "i";
+                    } else {
+                        // One is a float so result is a float register
+                        instructions.add("multimmf r" + (reg++) + "f " + termOne + " " + termTwo);
+                        symbols[nextSymbol(symbols, i)] = "r" + (reg - 1) + "f";
+                    }
+                    symbols[prevSymbol(symbols, i)] = "";
+                    symbols[i] = "";
+                } else if (termTwo.contains("r")) {
+                    // Two is a register
+                    if (termTwo.contains("i") && (Double.parseDouble(termOne) == (int) Double.parseDouble(termOne))) {
+                        // Both are ints so int operation
+                        instructions.add("multimmi r" + (reg++) + "i " + termTwo + " " + termOne);
+                        symbols[nextSymbol(symbols, i)] = "r" + (reg - 1) + "i";
+                    } else {
+                        // One is a float so result is a float register
+                        instructions.add("multimmf r" + (reg++) + "f " + termTwo + " " + termOne);
+                        symbols[nextSymbol(symbols, i)] = "r" + (reg - 1) + "f";
+                    }
+                    symbols[prevSymbol(symbols, i)] = "";
+                    symbols[i] = "";
+                } else {
+                    // Both are numbers
+                    if ((Double.parseDouble(termOne) == (int) Double.parseDouble(termOne)) && (Double.parseDouble(termTwo) == (int) Double.parseDouble(termTwo))) {
+                        // Both are ints so int operation
+                        instructions.add("multdubimmi r" + (reg++) + "i " + termOne + " " + termTwo);
+                        symbols[nextSymbol(symbols, i)] = "r" + (reg - 1) + "i";
+                    } else {
+                        // One is a float so result is a float register
+                        instructions.add("multdubimmf r" + (reg++) + "f " + termOne + " " + termTwo);
+                        symbols[nextSymbol(symbols, i)] = "r" + (reg - 1) + "f";
+                    }
+                    symbols[prevSymbol(symbols, i)] = "";
+                    symbols[i] = "";
                 }
-                symbols[i] = "";
-                symbols[i + 1] = "";
             } else if (symbols[i].equals("/")) {
-                try {
-                    Integer.parseInt(symbols[i - 1]);
-                    Integer.parseInt(symbols[i + 1]);
-                    instructions.add("divdubimmi r" + (reg++) + "i " + symbols[i - 1] + " " + symbols[i + 1]);
-                    symbols[i-1] = "r" + (reg - 1) + "i";
-                } catch (Exception e) {
-                    instructions.add("divdubimmf r" + (reg++) + "f " + symbols[i - 1] + " " + symbols[i + 1]);
-                    symbols[i-1] = "r" + (reg - 1) + "f";
+                notJustOne = true;
+                String termOne = symbols[prevSymbol(symbols, i)];
+                String termTwo = symbols[nextSymbol(symbols, i)];
+
+                if (termOne.contains("r") && termTwo.contains("r")) {
+                    // Both are registers
+                    if (termOne.contains("i") && termTwo.contains("i")) {
+                        // Both are int registers so int operation
+                        instructions.add("divi r" + (reg++) + "i " + termOne + " " + termTwo);
+                        symbols[nextSymbol(symbols, i)] = "r" + (reg - 1) + "i";
+                    } else {
+                        // One is a float register so result is a float reg.
+                        instructions.add("divf r" + (reg++) + "f " + termOne + " " + termTwo);
+                        symbols[nextSymbol(symbols, i)] = "r" + (reg - 1) + "f";
+                    }
+                    symbols[prevSymbol(symbols, i)] = "";
+                    symbols[i] = "";
+                } else if (termOne.contains("r")) {
+                    // One is a register
+                    if (termOne.contains("i") && (Double.parseDouble(termTwo) == (int) Double.parseDouble(termTwo))) {
+                        // Both are ints so int operation
+                        instructions.add("divimmi r" + (reg++) + "i " + termOne + " " + termTwo);
+                        symbols[nextSymbol(symbols, i)] = "r" + (reg - 1) + "i";
+                    } else {
+                        // One is a float so result is a float register
+                        instructions.add("divimmf r" + (reg++) + "f " + termOne + " " + termTwo);
+                        symbols[nextSymbol(symbols, i)] = "r" + (reg - 1) + "f";
+                    }
+                    symbols[prevSymbol(symbols, i)] = "";
+                    symbols[i] = "";
+                } else if (termTwo.contains("r")) {
+                    // Two is a register
+                    if (termTwo.contains("i") && (Double.parseDouble(termOne) == (int) Double.parseDouble(termOne))) {
+                        // Both are ints so int operation
+                        instructions.add("divimmi r" + (reg++) + "i " + termTwo + " " + termOne);
+                        symbols[nextSymbol(symbols, i)] = "r" + (reg - 1) + "i";
+                    } else {
+                        // One is a float so result is a float register
+                        instructions.add("divimmf r" + (reg++) + "f " + termTwo + " " + termOne);
+                        symbols[nextSymbol(symbols, i)] = "r" + (reg - 1) + "f";
+                    }
+                    symbols[prevSymbol(symbols, i)] = "";
+                    symbols[i] = "";
+                } else {
+                    // Both are numbers
+                    if ((Double.parseDouble(termOne) == (int) Double.parseDouble(termOne)) && (Double.parseDouble(termTwo) == (int) Double.parseDouble(termTwo))) {
+                        // Both are ints so int operation
+                        instructions.add("divdubimmi r" + (reg++) + "i " + termOne + " " + termTwo);
+                        symbols[nextSymbol(symbols, i)] = "r" + (reg - 1) + "i";
+                    } else {
+                        // One is a float so result is a float register
+                        instructions.add("divdubimmf r" + (reg++) + "f " + termOne + " " + termTwo);
+                        symbols[nextSymbol(symbols, i)] = "r" + (reg - 1) + "f";
+                    }
+                    symbols[prevSymbol(symbols, i)] = "";
+                    symbols[i] = "";
                 }
-                symbols[i] = "";
-                symbols[i + 1] = "";
             }
+//            if (symbols[i].equals("*")) {
+//                try {
+//                    Integer.parseInt(symbols[i - 1]);
+//                    Integer.parseInt(symbols[i + 1]);
+//                    instructions.add("multdubimmi r" + (reg++) + "i " + symbols[i - 1] + " " + symbols[i + 1]);
+//                    symbols[i-1] = "r" + (reg - 1) + "i";
+//                } catch (Exception e) {
+//                    instructions.add("multdubimmf r" + (reg++) + "f " + symbols[i - 1] + " " + symbols[i + 1]);
+//                    symbols[i-1] = "r" + (reg - 1) + "f";
+//                }
+//                symbols[i] = "";
+//                symbols[i + 1] = "";
+//            } else if (symbols[i].equals("/")) {
+//                try {
+//                    Integer.parseInt(symbols[i - 1]);
+//                    Integer.parseInt(symbols[i + 1]);
+//                    instructions.add("divdubimmi r" + (reg++) + "i " + symbols[i - 1] + " " + symbols[i + 1]);
+//                    symbols[i-1] = "r" + (reg - 1) + "i";
+//                } catch (Exception e) {
+//                    instructions.add("divdubimmf r" + (reg++) + "f " + symbols[i - 1] + " " + symbols[i + 1]);
+//                    symbols[i-1] = "r" + (reg - 1) + "f";
+//                }
+//                symbols[i] = "";
+//                symbols[i + 1] = "";
+//            }
         }
 
         // Just go in order
         for (i = 0; i < symbols.length; i++) {
             if (symbols[i].equals("+")) {
+                notJustOne = true;
                 String termOne = symbols[prevSymbol(symbols, i)];
                 String termTwo = symbols[nextSymbol(symbols, i)];
 
@@ -133,6 +285,7 @@ public class EvaluateExpression {
                     symbols[i] = "";
                 }
             } else if (symbols[i].equals("-")) {
+                notJustOne = true;
                 String termOne = symbols[prevSymbol(symbols, i)];
                 String termTwo = symbols[nextSymbol(symbols, i)];
 
@@ -192,15 +345,23 @@ public class EvaluateExpression {
             }
         }
         // Free up intermediate registers
-        if (symbols[symbols.length - 1].contains("i")) {
+        if (notJustOne && symbols[symbols.length - 1].contains("i") ) {
             instructions.add("movi r" + origReg + "i " + symbols[symbols.length - 1]);
-        } else {
+        } else if (notJustOne){
             instructions.add("movf r" + origReg + "f " + symbols[symbols.length - 1]);
+        } else {
+            // Only had one term in expression
+            if ((Double.parseDouble(symbols[0]) == (int) Double.parseDouble(symbols[0]))) {
+                // It was an int
+                instructions.add("movi r" + origReg + "i " + symbols[symbols.length - 1]);
+            } else {
+                // It was a double
+                instructions.add("movf r" + origReg + "f " + symbols[symbols.length - 1]);
+            }
         }
 
         reg = origReg + 1;
-        // TODO: Remove print
-        System.out.println(trueExpression);
+//        System.out.println(trueExpression);
         return instructions;
 
     }
@@ -222,7 +383,7 @@ public class EvaluateExpression {
     }
 
     public static void main (String[] args) throws Exception {
-        String ast = "(program let (declseg typedecls (vardecls (vardecl var (ids a , (ids b)) : (type int) (optinit := (const 0)) ;) vardecls) funcdecls) in (stmts (fullstmt (stmt if (boolexpr (clause (pred ( (boolexpr (clause (pred (numexpr (term (factor a))) (boolop =) (numexpr (term (factor b)))))) )))) then (stmts (fullstmt (stmt (lvalue a optoffset) := (numexpr (numexpr (numexpr (numexpr (term (factor b))) (linop +) (term (term (factor (const 2))) (nonlinop *) (factor (const 3)))) (linop +) (term (factor (const 4)))) (linop -) (term (factor (const 5))))) ;)) endif) ;) (stmts (fullstmt (stmt optstore printi ( (numexprs (neexprs (numexpr (term (factor a))))) )) ;))) end)";
+        String ast = "lvalue sum optoffset) := (numexpr (numexpr (term (factor sum))) (linop +) (term (term (factor x [ (numexpr (term (factor i))) ])) (nonlinop *) (factor y [ (numexpr (term (factor i))) ])))) ;))";
         String expression = ast.substring(ast.indexOf("lvalue"));
         ArrayList<String> instructions = evaluateExpression(expression, 0);
         System.out.println(instructions);
